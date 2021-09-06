bd5957401a39dbdbfa5a88889fec1e1ab63ef57a
Danny Shen <junwu.shen@anchortech.io>
2021-09-06 19:24:36 +0800

    Initial Commit

---
+++ QRCodeDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeDemo
+//
+//  Created by 申潤五 on 2021/9/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
